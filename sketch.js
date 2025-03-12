d8b93ce516bdcc55074fcec0cfafb504bed442c3
izzyyadams <ipa2010@nyu.edu>
2025-03-12 17:58:59 -0400

    one moving ball

--- sketch.js
+++ sketch.js
@@ -1,28 +1,72 @@
+let oneLava;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
-  colorMode(HSL);
+  colorMode(HSL,360, 100, 100, 1);
+  oneLava = new LavaBlob(200, 355, 99, 400, 400, true);
+
 }
 
 function draw() {
-  danceFloorGrid(100);
+  lavaLamp();
   
+
+}
+
+function lavaLamp() {
+  let lavaLampBackground = color(306, 100, 16, 1);
+  background(lavaLampBackground);
+  oneLava.update();
+  oneLava.display();
+
+  
+
+
 }
 
 
-function danceFloorGrid(squareSize) {
-  let purple = color(261, 98, 80);
-  let orange = color(21, 98, 54);
-  let green = color(119, 99,71);
-  let yellow = color(48, 98, 51);
-  let black = color(0);
-  let colors = [purple, orange, green, yellow];
-  for(i = 0; i < windowHeight/squareSize; i++) {
-    for (j = 0; j < windowWidth/squareSize; j++) {
-      fill(black);
-      square(j * squareSize, i *squareSize, squareSize);
-      
+//Creates one lava blob
+//initital radius is the spawn radius
+//centerColorH and CenterColorS are used to decide initial hue and saturation of blob, lightness changed in gradient loop
+//startX, startY -> initial coordinates
+//up -> if starts moving up or down. 
+class LavaBlob {
+  constructor(initialRadius, centerColorH, centerColorS, x, y, up){
+    this.initialRadius = initialRadius;
+    this.centerColorH = centerColorH;
+    this.centerColorS = centerColorS;
+    this.radius = this.initialRadius;
+    this.x = x;
+    this.y = y;
+    this.up = up;
+    
+  }
+
+  update() {
+    if (this.up) {
+      this.y -= 1;
+    }else{
+      this.y += 1;
+    }
+    if (this.y - this.radius/2 <= 0) {
+      this.up = false;
+    }
+    if (this.y + this.radius/2  >= windowHeight) {
+      this.up = true;
     }
   }
-}
+
+  display () {
+    noFill();
+    
+    
+    for (let i = 0; i < this.initialRadius; i ++) {
+      let tempColor = color(this.centerColorH, this.centerColorS, map(i, this.initialRadius, 0, 25, 50), 1);
+      stroke(tempColor);
+      circle(this.x, this.y, i);
+    }
+  }
+
+
   
+}
+
